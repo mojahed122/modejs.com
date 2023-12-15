2a8af29036081b1bf6fc696603f5612d7027ef98
mojahed <mojahed8822@gmail.com>
2023-12-14 21:50:12 -1200

    one

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-document.querySelector(".side-bar").onclick = function() {
-    document.querySelector("show").classList.toggle("open");
-}
+const sideBar = document.querySelector(".side-bar");
+const headerMenuo = document.querySelector(".header-menuo");
+
+sideBar.addEventListener("click", () => {
+    sideBar.classList.toggle("active");
+    headerMenuo.classList.toggle("active");
+})
+
+
+
+
+
+
+
+
+
+// document.querySelector(".side-bar").onclick = function() {
+//     document.querySelector("show").classList.toggle("open");
+// }
